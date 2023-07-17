1fed92cd30e6654e418b8e4461742054c9641fe7
Quoc Gia <67970731+quocgiahcmut@users.noreply.github.com>
2023-07-17 19:53:29 +0700

    first commit

---
+++ src/Services/Catalog/Catalog.Api/Models/CatalogItem.cs
@@ -0,0 +1,11 @@
+namespace Catalog.Api.Models;
+
+public class CatalogItem
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public decimal Price { get; set; }
+    public string PictureFileName { get; set; }
+    public string PictureUri { get; set; }
+}
---
+++ src/Services/Catalog/Catalog.Api/Models/CatalogType.cs
@@ -0,0 +1,7 @@
+namespace Catalog.Api.Models;
+
+public class CatalogType
+{
+    public int Id { get; set; }
+    public string Type { get; set; }
+}
