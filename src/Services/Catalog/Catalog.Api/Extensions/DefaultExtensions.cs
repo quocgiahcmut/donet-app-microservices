e49fccf2213787f5dba3d4fe2c1a0a4ce428d74e
Quoc Gia <67970731+quocgiahcmut@users.noreply.github.com>
2023-07-18 17:12:51 +0700

    .

---
+++ src/Services/Catalog/Catalog.Api/Extensions/DefaultExtensions.cs
@@ -0,0 +1,6 @@
+namespace dotnet.microservices.Services.Catalog.Api.Extensions;
+
+public static class DefaultExtensions
+{
+
+}
---
+++ src/Services/Common/CommonExtensions.cs
@@ -0,0 +1,9 @@
+﻿namespace dotnet.microservices.Services.Common;
+
+public class CommonExtensions
+{
+    public static WebApplicationBuilder AddServiceDefaults(this WebApplicationBuilder builder)
+    {
+
+    }
+}
